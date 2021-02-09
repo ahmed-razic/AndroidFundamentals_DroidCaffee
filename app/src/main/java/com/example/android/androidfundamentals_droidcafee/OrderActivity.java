4aa49aa8956372b80812a67b03ed28e5cce1208a
ahmed-razic <ahmed.razic@gmail.com>
2021-02-09 12:12:21 +0100

    added radio buttons

--- app/src/main/java/com/example/android/androidfundamentals_droidcafee/OrderActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_droidcafee/OrderActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
@@ -16,7 +17,24 @@ public class OrderActivity extends AppCompatActivity {
 
     public void onRadioButtonClicked(View view) {
 
+        boolean checked = ((RadioButton) view).isChecked();
 
+        switch (view.getId()){
+            case R.id.sameday_radioButton:
+                if(checked)
+                    displayToast(getString(R.string.same_day_messenger_service));
+                break;
+            case R.id.nextday_radioButton:
+                if(checked)
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup_radioButton:
+                if(checked)
+                    displayToast(getString(R.string.pickup));
+                break;
+            default :
+                break;
+        }
     }
 
     public void displayToast(String message) {
