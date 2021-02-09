0e4690ae5e82747e9456449081203496a914fe46
ahmed-razic <ahmed.razic@gmail.com>
2021-02-09 11:59:43 +0100

    added different EditText views

--- app/src/main/java/com/example/android/androidfundamentals_droidcafee/OrderActivity.java
+++ app/src/main/java/com/example/android/androidfundamentals_droidcafee/OrderActivity.java
@@ -3,6 +3,8 @@ package com.example.android.androidfundamentals_droidcafee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class OrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+    }
+
+    public void onRadioButtonClicked(View view) {
+
+
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
